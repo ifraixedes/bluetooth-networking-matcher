42403ff72e312964a4e2787548468387dc08fcad
Ivan Fraixedes Cugat <ifcdev@gmail.com>
2012-09-15 17:09:07 +0000

    Last implementations to arrive to hackathon end-time

--- impl/app/src/org/hispanos/btnetworkingmatcher/app/storage/BluetoothDAO.java
+++ impl/app/src/org/hispanos/btnetworkingmatcher/app/storage/BluetoothDAO.java
@@ -1,5 +1,8 @@
 package org.hispanos.btnetworkingmatcher.app.storage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.bluetooth.BluetoothDevice;
 import android.content.ContentValues;
 import android.database.Cursor;
@@ -18,6 +21,25 @@ public class BluetoothDAO {
 		database = ddbb;
 	}
 
+	// TODO Implemented to arrive to the hackathon end-time -- REFACTORING
+	public ArrayList<HashMap<String, String>> getMatchedDevicesInfo() {
+
+		Cursor queryCursor = database.query(_BT_DEVICES_TABLE_NAME, null, null,
+				null, null, null, null);
+
+		ArrayList<HashMap<String, String>> devicesInfo = new ArrayList<HashMap<String, String>>();
+		HashMap<String, String> deviceInfo;		
+		
+		while (queryCursor.moveToNext()) {
+			deviceInfo = new HashMap<String, String>();
+			deviceInfo.put("MAC ADDRESS", queryCursor.getString(0));
+			deviceInfo.put("PUBLIC NAME", queryCursor.getString(1));
+			deviceInfo.put("NUMBER OF MATCHES", Integer.toString(queryCursor.getInt(2)));
+		}
+		
+		return devicesInfo;
+	}
+
 	public int getDevicesMatches(BluetoothDevice device) {
 		Cursor queryCursor = database.query(_BT_DEVICES_TABLE_NAME,
 				new String[] { "_BT_DEVICES_COL_MATCHES_COUNTER" },
@@ -33,20 +55,18 @@ public class BluetoothDAO {
 	}
 
 	public void updateDeviceInfo(BluetoothDevice device) {
-		
+
 		int deviceMatches = getDevicesMatches(device);
-		
+
 		ContentValues btFieldsAndValues = new ContentValues();
-		btFieldsAndValues
-				.put(_BT_DEVICES_COL_MAC_ADDR, device.getAddress());
-		btFieldsAndValues
-				.put(_BT_DEVICES_COL_PUBLIC_NAME, device.getName());
+		btFieldsAndValues.put(_BT_DEVICES_COL_MAC_ADDR, device.getAddress());
+		btFieldsAndValues.put(_BT_DEVICES_COL_PUBLIC_NAME, device.getName());
 
 		if (deviceMatches > 0) { // Update
 
 			btFieldsAndValues.put(_BT_DEVICES_COL_MATCHES_COUNTER,
 					++deviceMatches);
-			
+
 			database.update(_BT_DEVICES_TABLE_NAME, btFieldsAndValues,
 					_BT_DEVICES_COL_MAC_ADDR + "='?'",
 					new String[] { device.getAddress() });

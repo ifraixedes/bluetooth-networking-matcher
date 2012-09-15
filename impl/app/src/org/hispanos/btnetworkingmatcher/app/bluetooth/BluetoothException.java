70107115054a6514794b0c0c27eb265fcb1b4640
Ivan Fraixedes Cugat <ifcdev@gmail.com>
2012-09-15 13:59:53 +0000

    Coding the bluetooth operations. Partial development.

---
+++ impl/app/src/org/hispanos/btnetworkingmatcher/app/bluetooth/BluetoothException.java
@@ -0,0 +1,13 @@
+package org.hispanos.btnetworkingmatcher.app.bluetooth;
+
+public class BluetoothException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BluetoothException(String msg) {
+		super(msg);
+	}
+}
--- impl/app/src/org/hispanos/btnetworkingmatcher/app/bluetooth/Sniffer.java
+++ impl/app/src/org/hispanos/btnetworkingmatcher/app/bluetooth/Sniffer.java
@@ -1,9 +1,74 @@
 package org.hispanos.btnetworkingmatcher.app.bluetooth;
 
-public class Sniffer {
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 
-	public void fetchDeveices() {
+public class Sniffer {
+	
+	protected Context appContext;
+	protected BluetoothAdapter btAdpater;
+	
+	// Local BroadcastReceivers
+	protected DiscoveryStartedReceiver disStartedRecvr = new DiscoveryStartedReceiver();
+	protected DiscoveryFinishedReceiver disfinishedRecvr = new DiscoveryFinishedReceiver();
+	protected FoundReceiver foundRecvr = new FoundReceiver();
+	
+	
+
+	public Sniffer(Context context) {
+		appContext = context;
+		btAdpater = BluetoothAdapter.getDefaultAdapter();
+		
+		// Initialise the local BroadcastReceivers
+		
 		
 	}
+	
+	public void fetchDeveices() throws BluetoothException {
+		
+		if ((!btAdpater.cancelDiscovery()) || (!btAdpater.startDiscovery())) {
+			throw new BluetoothException("The bluetooth device is turn off");
+		}
+		
+		
+	}
+	
+	
+	protected void initLocalBroadcastReceivers() {
+		
+	}
+	
+	protected class DiscoveryStartedReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	protected class DiscoveryFinishedReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	protected class FoundReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
 	
 }

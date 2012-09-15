6106df6c4fa589e9dc527ddff57ac8cbbc82579e
SeRPRo <serpro@gmail.com>
2012-09-15 14:47:21 +0100

    BluetoothService

---
+++ impl/app/src/org/hispanos/btnetworkingmatcher/app/service/BluetoothService.java
@@ -0,0 +1,87 @@
+package org.hispanos.btnetworkingmatcher.app.service;
+
+import org.hispanos.btnetworkingmatcher.app.R;
+import org.hispanos.btnetworkingmatcher.app.bluetooth.Sniffer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class BluetoothService extends Service {
+
+	private Sniffer btSniffer;
+	private SharedPreferences preferences;
+
+	static String LOG_TAG = "BT_SERVICE";
+
+	/**
+	 * Class for clients to access.  Because we know this service always
+	 * runs in the same process as its clients, we don't need to deal with
+	 * IPC.
+	 */
+	public class LocalBinder extends Binder {
+		BluetoothService getService() {
+			return BluetoothService.this;
+		}
+	}
+
+	// This is the object that receives interactions from clients.  See
+	// RemoteService for a more complete example.
+	private final IBinder mBinder = new LocalBinder();
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return mBinder;
+	}
+
+	@Override
+	public void onCreate() {
+		btSniffer = new Sniffer();
+		preferences = getApplicationContext().getSharedPreferences(getString(R.string.prefs_name),0);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i(LOG_TAG, "Received start id " + startId + ": " + intent);
+		preferences.edit()
+		.putBoolean(getString(R.string.service), true)
+		.commit();
+
+		final Resources res = getResources();
+
+		new Thread(new Runnable(){
+			public void run() {
+
+				String var_delay = res.getString(R.string.pref_delay);
+				String default_delay = res.getStringArray(R.array.pref_delay_values)[0];
+				int delay = Integer.parseInt(preferences.getString(var_delay, default_delay)) * 1000;
+				while(true)
+				{
+					btSniffer.fetchDeveices();
+					try {
+						Thread.sleep(delay);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}).start();
+
+		// We want this service to continue running until it is explicitly
+		// stopped, so return sticky.
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		preferences.edit()
+		.putBoolean(getString(R.string.service), false)
+		.commit();
+	}
+}
